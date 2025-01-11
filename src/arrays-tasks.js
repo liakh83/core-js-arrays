0f49c499d8a3447ecdecaa9af6b069decd3734a1
liakh83 <lahn4573@gmail.com>
2025-01-11 10:18:37 +0300

    feat: add solution for isValueEqualsIndex, insertItem

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -164,8 +164,11 @@ function isSameLength(arr) {
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  const even = (item) => {
+    return item === arr.indexOf(item);
+  };
+  return arr.some(even);
 }
 /**
  * Inserts the item into specified array at specified index.
@@ -178,8 +181,8 @@ function isValueEqualsIndex(/* arr */) {
  *    insertItem([ 1, 3, 4, 5 ], 2, 1)  => [ 1, 2, 3, 4, 5 ]
  *    insertItem([ 1, 'b', 'c'], 'x', 0) => [ 'x', 1, 'b', 'c' ]
  */
-function insertItem(/* arr, item, index */) {
-  throw new Error('Not implemented');
+function insertItem(arr, item, index) {
+  return arr.splice(index, 0, item);
 }
 
 /**
